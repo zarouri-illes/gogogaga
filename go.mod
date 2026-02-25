2eb46fadba1fa2b2e4f0b493f237bd4a5587d59e
illes <illeszarouri@gmail.com>
2026-02-25 02:20:04 +0100

    config router using chi package

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dreamsofcode-io/orders-api
 
 go 1.25.0
+
+require github.com/go-chi/chi/v5 v5.2.5
--- main.go
+++ main.go
@@ -3,12 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+
+	router := chi.NewRouter()
+
+	router.Use(middleware.Logger)
+	router.Get("/api", hndler)
+
 	server := &http.Server{
 		Addr:    ":3000",
-		Handler: http.HandlerFunc(hndler),
+		Handler: router,
 	}
 
 	err := server.ListenAndServe()
